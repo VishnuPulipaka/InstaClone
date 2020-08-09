4997f1bb3a39234a8c46203d9698aa0d19d42fe5
Vishnu <vishnujps64@gmail.com>
2020-08-09 11:48:59 +0530

    Post model set up

--- instaClone/insta/models.py
+++ instaClone/insta/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class Post(models.Model):
+	author= models.ForeignKey('auth.User',on_delete=models.CASCADE)
+	image=models.ImageField(blank=True,null=True)
+	caption=models.CharField(max_length=255)
+	crated_time=models.DateTimeField(default=timezone.now)
